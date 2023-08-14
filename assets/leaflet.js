6bddfcc7d65d12ca68bed28adcd98160eb726813
Elizeu <el_vailant@hotmail.com>
2023-08-14 10:26:47 -0400

    tentando passar o mapa pela função

--- assets/leaflet.js
+++ assets/leaflet.js
@@ -1,62 +1,5 @@
-import React, { useState, useRef, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
-import { WebView } from "react-native-webview";
-import NavigationView from "./NavigationView";
-import { readAsStringAsync } from "expo-file-system";
-import { useAssets } from "expo-asset";
-import * as Location from "expo-location";
-// import * as mapaLeaflet from "../assets/leaflet";
-
-const ContentView = () => {
-  const webViewRef = useRef();
-  const [canGoBack, setCanGoBack] = useState(false);
-  const [canGoForward, setCanGoFoward] = useState(false);
-  const [html, setHtml] = useState("");
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  // pega o arquivo html
-  // const [index, indexLoadingError] = useAssets(
-  //   require("../assets/leaflet.html")
-  // );
-
-  // if (index) {
-  //   readAsStringAsync(index[0].localUri).then((data) => {
-  //     setHtml(data);
-  //   });
-  // }
-
-  // const handleBackPress = () => {
-  //   webViewRef.current.goBack();
-  // };
-
-  // const handleForwardPress = () => {
-  //   webViewRef.current.goForward();
-  // };
-
-  // pega a localizacao
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  let mostrarMapa = false;
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-
-    // script para enviar latitude e longitude para a pagina html
-    const mapaLocUsuario = `
+const mapaLealet = (lat, long) => {
+  return `
     <!DOCTYPE html>
     <html lang="en">
       <head>
@@ -104,8 +47,8 @@ const ContentView = () => {
           // const lat = 51.511948;
           // const long = -0.088749;  
           
-          const lat = ${location.latitude};
-          const long = ${location.longitude};
+          const lat = ${lat};
+          const long = ${long};
 
           var map = L.map("map").setView([lat, long], 13);
 
@@ -157,40 +100,6 @@ const ContentView = () => {
       </body>
     </html>
     `;
-
-    setHtml(mapaLocUsuario);
-  }
-
-  return (
-    <View style={styles.container}>
-      <WebView
-        ref={webViewRef}
-        source={{ html }}
-        originWhitelist={["*"]}
-        onNavigationStateChange={(state) => {
-          const back = state.canGoBack;
-          const forward = state.canGoForward;
-          setCanGoBack(back);
-          setCanGoFoward(forward);
-        }}
-      />
-
-      <Text>{text}</Text>
-      {/* <NavigationView
-        onBackPress={handleBackPress}
-        onForwardPress={handleForwardPress}
-        canGoBack={canGoBack}
-        canGoForward={canGoForward}
-      /> */}
-    </View>
-  );
 };
 
-export default ContentView;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 10,
-  },
-});
+export default mapaLealet;
