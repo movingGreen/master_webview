cc4a2abb644de0b1a3660adcf6eff9a2d10c2a45
Paulo Roberto <61876313+Maximuzsz@users.noreply.github.com>
2023-09-14 18:37:40 -0300

    correção da visualização do mapa e do poligono

--- src/MapaLeaflet.js
+++ src/MapaLeaflet.js
@@ -9,6 +9,21 @@ const MapaLeaflet = ({ latitude, longitude }) => {
   const [latitudeStateWebview, setLatitudeStateWebview] = useState(latitude);
   const [longitudeStateWebview, setLongitudeStateWebview] = useState(longitude);
 
+  let webRef;
+
+  const marker = (lat, long, texto) => {
+    let textoMarker = texto && `<b>Hello world!</b><br />I am a popup.`;
+
+    return `
+      
+      var marker = L.marker([${lat}, ${long}])
+      .addTo(map)
+      .bindPopup(${textoMarker})
+      .openPopup();
+
+    `;
+  };
+
   const html = `
     <!DOCTYPE html>
     <html lang="en">
@@ -17,11 +32,17 @@ const MapaLeaflet = ({ latitude, longitude }) => {
         <meta
           name="viewport"  
           content="width=device-width, initial-scale=1" />
-
-          <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin=""/>
-
-          <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js" integrity="sha256-20nQCchB9co0qIjJZRGuk2/+kNiyxNV1lvTlZBo=" crossorigin=""></script>
-
+    
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+          integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+          crossorigin="" />
+        <script
+          src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+          integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+          crossorigin=""></script>
+    
         <style>
           html,
           body {
@@ -42,8 +63,8 @@ const MapaLeaflet = ({ latitude, longitude }) => {
           style="width: 100%; height: 100%">
         </div>
         <script>            
-            const lat = ${JSON.stringify(latitudeStateWebview)}
-            const long = ${JSON.stringify(longitudeStateWebview)}
+            const lat = ${latitudeStateWebview}
+            const long = ${longitudeStateWebview}
             
             var map = L.map("map").setView([lat, long], 18);
             
@@ -55,40 +76,38 @@ const MapaLeaflet = ({ latitude, longitude }) => {
               '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
             }
             ).addTo(map);
-            
+
             var circle = L.circle([lat + 0.04, long ], {
               color: "red",
               fillColor: "#f03",
-            fillOpacity: 0.5,
-            radius: 500,
-          })
-          .addTo(map)
-          .bindPopup("I am a circle.");
-          
-          var polygon = L.polygon([
-            [lat + 0.02, long - 0.02],
-            [lat + 0.03, long ],
-            [lat + 0.01, long ],
-            [lat + 0.012, long - 0.01],
-          ])
-            .addTo(map)
-            .bindPopup("I am a polygon.");
+              fillOpacity: 0.5,
+              radius: 500,
+            })
+              .addTo(map)
+              .bindPopup("I am a circle.");
             
+            var polygon = L.polygon([
+              [lat + 0.02, long - 0.02],
+              [lat + 0.03, long ],
+              [lat + 0.01, long ],
+              [lat + 0.012, long - 0.01],
+            ])
+              .addTo(map)
+              .bindPopup("I am a polygon.");
+
             var popup = L.popup()
-            .setLatLng([lat, long])
-            .setContent("I am a standalone popup.")
-            .openOn(map);
-            
+              .setLatLng([lat, long])
+              .setContent("I am a standalone popup.")
+              .openOn(map);
+
             function onMapClick(e) {
               popup
               .setLatLng(e.latlng)
               .setContent("You clicked the map at " + e.latlng.toString())
               .openOn(map);
-            }
-            
+            }  
+
             map.on("click", onMapClick);
-            return map
-          
           </script>
           </body>
     </html>`;
@@ -101,15 +120,16 @@ const MapaLeaflet = ({ latitude, longitude }) => {
     );
   }, [latitudeState, longitudeState]);
 
-  // useEffect(() => {
-  //   webRef.reload();
-  // }, [mostraMapa]);
+  useEffect(() => {
+    webRef.reload();
+  }, [mostraMapa]);
 
   return (
     <>
       <WebView
         source={{ html }}
         originWhitelist={["*"]}
+        ref={(r) => (webRef = r)}
         onMessage={(event) => {}}
       />
       <Text>Latitude: {latitudeStateWebview}</Text>
